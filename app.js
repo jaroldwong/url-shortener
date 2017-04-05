a898dbb9b069e7c6558a5e9fe48d91055b461b72
Jarold Wong <jaroldw@gmail.com>
2017-04-04 19:26:08 -0700

    accept original URL parameter

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ app.get('/', function (req, res) {
   res.send('URL Shortener');
 });
 
+// I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
+// When I visit that shortened URL, it will redirect me to my original link.
+app.get('/*', function (req, res) {
+  var url = req.params[0];
+  var shurl = req.hostname;
+  
+  // if invalid URL, return error
+  
+  var data = {
+    "original_url": url,
+    "short_url": shurl
+  };
+
+  var error = {
+    "error": "URL invalid"
+  };
+  
+  res.send(JSON.stringify(data));
+});
+
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
 });
